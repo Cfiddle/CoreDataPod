ea07c36d746f25c738635dbf6ac513a1a091f39e
Charles Fiedler <chase.fiedler@skyward.io>
2018-10-09 13:37:26 -0700

    Initial Commit

---
+++ CoreDataPod/CoreDataPod.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataPod.h
+//  CoreDataPod
+//
+//  Created by Charles Fiedler on 10/9/18.
+//  Copyright © 2018 MKD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataPod.
+FOUNDATION_EXPORT double CoreDataPodVersionNumber;
+
+//! Project version string for CoreDataPod.
+FOUNDATION_EXPORT const unsigned char CoreDataPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataPod/PublicHeader.h>
+
+
